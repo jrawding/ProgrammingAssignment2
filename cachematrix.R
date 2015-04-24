b2e19a9974e545ff4570db7b6109987a7293b45e
jrawding <jennifer.m.rawding@gmail.com>
2015-04-24 16:21:21 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+### This code caches the potentially time consuming inverse matrix calculation, so that if the matrix is
+### unchanging, the inverse matrix can be looked up rather than calculated
 
-## Write a short comment describing this function
+### NOTE: This code assumes use of square matrices, to code for a non-square matrix, insert a line in the
+### makeCacheMatrix function to load the MASS package (library(MASS)) and replace solve with ginv throughout
+
+## This function creates a list of functions to set/get the value of the matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+            set <- function(y){
+                x <<- y
+                m <<- NULL
+            }
+            get <- function() x
+            setMat <- function(solve) m <<- solve
+            getMat <-function() m
+            list(set = set, get = get, 
+                 setMat = setMat, 
+                 getMat = getMat)
 }
 
 
-## Write a short comment describing this function
+## This function checks to see if the inverse matrix has already been calculated; if yes, it gets the mean from
+## the cache instead of calculating; if no, it calculates the inverse of the data and updates the cache via setMat
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getMat()
+            if(!is.null(m)){
+                message("getting cached data")
+                return(m)
+            }
+            data <-x$get()
+            m <- solve(data,...)
+            x$setMat(m)
+            m
 }
